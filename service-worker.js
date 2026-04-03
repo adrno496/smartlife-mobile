30b5c36cbbbb3bc3d7478aba3607e6fe733688bb
DREANO <dreano@MacBook-Air-de-DREANO.local>
2026-04-04 00:55:14 +0200

    fix cache pwa

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,16 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html"
-      ]);
-    })
-  );
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => caches.delete(key)))
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
 });
